c7280d767a90f0ba1ea2d8bc81d8fb01e59bba18
Animesh Mahata <imanimesh1210@gmail.com>
2024-11-29 09:01:25 +0530

    Removed unnecessary import

--- src/main/java/com/animesh/rbacs/service/JwtService.java
+++ src/main/java/com/animesh/rbacs/service/JwtService.java
@@ -4,7 +4,6 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.KeyGenerator;
